7d9b3f37cc067debbc1d3339e6cdd34c54f11b13
DimitarKisov <mitko9737@gmail.com>
2021-10-21 22:54:34 +0300

    Added validator for EditArticleCommand

---
+++ Body4U.Application/Features/Articles/Commands/Edit/EditArticleCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿namespace Body4U.Application.Features.Articles.Commands.Edit
+{
+    using FluentValidation;
+
+    using static Body4U.Domain.Models.ModelConstants.Article;
+
+    public class EditArticleCommandValidator : AbstractValidator<EditArticleCommand>
+    {
+        public EditArticleCommandValidator()
+        {
+            this.RuleFor(x => x.Title)
+                .MinimumLength(MinTitleLength)
+                .MaximumLength(MaxTitleLength)
+                .NotEmpty();
+
+            this.RuleFor(x => x.Image)
+                .NotEmpty();
+
+            this.RuleFor(x => x.Content)
+                .MinimumLength(MinContentLength)
+                .NotEmpty();
+        }
+    }
+}
